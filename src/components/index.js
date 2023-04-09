87028b0f65608f973f93612e700c0da65a157f82
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-04-10 02:12:53 +0530

    file name change

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import CustomButton from "./CustomButton";
 import FilePicker from "./FilePicker";
 import ColorPicker from "./ColorPicker";
-import AiPicker from "./AiPicker";
+import AiPicker from "./AIPicker";
 import Tab from "./Tab";
 
 export { CustomButton, FilePicker, ColorPicker, AiPicker, Tab };
